6fdb613a73f1874128ce6702b27d77d73810a024
Steve Chipman <steven.chipman@yahooinc.com>
2025-05-29 11:11:10 -0400

    Initial Commit

---
+++ SimpleSocial/SimpleSocialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleSocialApp.swift
+//  SimpleSocial
+//
+//  Created by Steven Chipman on 5/29/25.
+//
+
+import SwiftUI
+
+@main
+struct SimpleSocialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SimpleSocialTests/SimpleSocialTests.swift
@@ -0,0 +1,16 @@
+//
+//  SimpleSocialTests.swift
+//  SimpleSocialTests
+//
+//  Created by Steven Chipman on 5/29/25.
+//
+
+import Testing
+
+struct SimpleSocialTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
